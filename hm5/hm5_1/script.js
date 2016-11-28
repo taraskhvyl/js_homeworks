255bf6684c44455dbaf059ff22f714e0f44b32ce
Taras <taras_hvil@mail.ru>
2016-11-28 11:57:14 +0200

    поправил промис

--- hm5/hm5_1/script.js
+++ hm5/hm5_1/script.js
@@ -1,16 +1,9 @@
 let timer = function (seconds) {
-	return {
-		then : 
-			function (result) {
-				let promise = new Promise((resolve) => {
-					setTimeout(() => {
-						resolve();
-					}, seconds);
-				});
-			
-				promise.then(result);
-			}
-	}
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();
+		}, seconds); 
+	});
 }
 
 timer(3000).then(() => console.log('я вывелась через 3 секунды'));
