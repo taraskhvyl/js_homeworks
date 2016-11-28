db39ee2148a2981554635c713c95b854f3989c22
Taras <taras_hvil@mail.ru>
2016-11-28 17:04:54 +0200

    исправил конфликт

--- hm5/hm5_1/script.js
+++ hm5/hm5_1/script.js
@@ -1,25 +1,8 @@
 let timer = function (seconds) {
-<<<<<<< HEAD
-	return {
-		then : 
-			function (result) {
-				let promise = new Promise((resolve) => {
-					setTimeout(() => {
-						resolve();
-					}, seconds);
-				});
-			
-				promise.then(result);
-			}
-	}
-}
-
-=======
 	return new Promise((resolve) => {
 		setTimeout(() => {
 			resolve();
 		}, seconds); 
 	});
 }
->>>>>>> hm5_1
 timer(3000).then(() => console.log('я вывелась через 3 секунды'));
